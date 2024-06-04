934ced104b69e92738703b14e69cc91f98c13f7f
Rohit Mandhyan <rmandhyan54@gmail.com>
2024-06-04 23:57:23 +0530

    completed fourth div in the HTML page

--- docs/assets/js script/script.js
+++ docs/assets/js script/script.js
@@ -1,54 +1,15 @@
-// const slider = document.querySelector('.slider');
-// const radioButtons = document.querySelectorAll('.radio-buttons img');
+document.addEventListener('DOMContentLoaded', () => {
+  const hoverItems = document.querySelectorAll('.hover-item');
+  const mainImage = document.getElementById('main-image');
 
-// let currentIndex = 0;
-
-// function updateSlider() {
-//     const translateX = -currentIndex * 370; // Adjust based on image width
-//     slider.style.transform = `translateX(${translateX}px)`;
-// }
-
-// radioButtons.forEach((button, index) => {
-//     button.addEventListener('click', () => {
-//         currentIndex = index;
-//         updateSlider();
-//     });
-// });
-
-// // Initialize slider
-// updateSlider();
-
-const slider = document.querySelector('.slider');
-  const radioButtons = document.querySelectorAll('.radio-buttons img');
-
-  let currentIndex = 0;
-
-  function updateSlider() {
-    const translateX = -currentIndex * 400; // Adjust based on image width + margin
-    slider.style.transform = `translateX(${translateX}px)`;
-
-    // Update active class on radio buttons
-    radioButtons.forEach((button, index) => {
-      if (index === currentIndex) {
-        button.classList.add('active');
-      } else {
-        button.classList.remove('active');
-      }
-    });
-  }
-
-  radioButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-      currentIndex = index;
-      updateSlider();
-    });
+  hoverItems.forEach(item => {
+      item.addEventListener('mouseover', () => {
+          const newImage = item.getAttribute('data-image');
+          mainImage.src = newImage;
+      });
   });
+});
 
-  // Initialize slider
-  updateSlider();
-
-  // checkbox checked or not for submit button
-  // https://www.kodyaz.com/articles/javascript-enable-disable-button-by-values-of-checkboxes-checked.aspx
 
   // https://codepen.io/jasonnarciso/pen/JdVPOW
   $('#checkbox').click(function() {
@@ -58,3 +19,5 @@ const slider = document.querySelector('.slider');
       $('#submit').attr('disabled', 'disabled');
     }
   });
+
+
